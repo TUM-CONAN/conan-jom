b7c3165c8b6a769701043638f77c85863b5b7c64
julien.waechter <julien.waechter@gmail.com>
2018-10-05 16:28:39 +0200

    chore(conanfile): clean gitlab-ci script

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ from conans.model.version import Version
 
 if __name__ == "__main__":
     if tools.os_info.is_windows:
-        builder = ConanMultiPackager(username="fw4spl")
+        builder = ConanMultiPackager(username="sight")
         builder.add({"os_build": "Windows", "arch_build": "x86"}, {}, {}, {})
         builder.add({"os_build": "Windows", "arch_build": "x86_64"}, {}, {}, {})
         builder.run()
